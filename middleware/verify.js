ba8863be2836c17cafe2297779539431c73c5f07
yangpu <18312701884@163.com>
2019-03-06 22:01:02 +0800

    feat：清除一些注释

--- middleware/verify.js
+++ middleware/verify.js
@@ -4,26 +4,20 @@ const config = require('../config')
 const VerifyToken = async(ctx, next) => {
   const authorization = ctx.get('Authorization');
   const url = ctx.path
-  console.log('来了', url)
-  console.log('token', authorization)
 
   if (!authorization) {
     ctx.throw(401, 'no token detected in http header \'Authorization\'');
   }
 
   if (url.indexOf('/users') > -1) {
-    console.log('不需要token')
     await next()
   } else {
-    console.log('需要token')
     try {
       const tokenContent = await jwt.verify(authorization, config.secret);
       console.log(tokenContent)
       console.log(tokenContent.userId)
       ctx.userId = tokenContent.userId
-      console.log('鉴权成功');
     } catch (error) {
-      console.log(error.name)
       if ('TokenExpiredError' === error.name) {
           ctx.throw(401, 'token expired！');
       }

5fdfc90648c7d18777efba8ec878ffc4159ec2c4
yangpu <yangpu@xlink.com>
2019-03-06 10:32:11 +0800

    添加路由，post的解析

--- index.js
+++ index.js
@@ -1,8 +1,27 @@
-const Koa = require('koa');
-const app = new Koa();
+const Koa = require('koa')
+const router = require('koa-router')()
+const bodyParser = require('koa-bodyparser')
+const app = new Koa()
 
-app.use(async ctx => {
-  ctx.body = 'Hello World';
-});
 
-app.listen(3000);
+const about = ctx => {
+  console.log('about')
+  ctx.response.type = 'html'
+  ctx.response.body = '<a href="/">Index Page</a>'
+}
+
+const main = ctx => {
+  console.log('main')
+  ctx.response.body = 'Hello World'
+}
+
+
+// app.use中间件，全局只有app.use的功能
+
+router.get('/', main)
+router.get('/about', about)
+
+app.use(bodyParser())
+app.use(router.routes())
+
+app.listen(3000)

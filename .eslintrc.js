46ec2e33ecc4542f81aba99bb1d18a1d814e5818
yangpu <18312701884@163.com>
2019-03-19 23:38:47 +0800

    '修复eslint的bug'

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,29 +1,18 @@
 module.exports = {
-    'env': {
-        'browser': true,
-        'commonjs': true,
-        'es6': true
-    },
-    'extends': 'eslint:recommended',
-    'parserOptions': {
-        'sourceType': 'module'
-    },
-    'rules': {
-        'indent': [
-            'error',
-            2
-        ],
-        'linebreak-style': [
-            'error'
-        ],
-        'no-console': 'off',
-        'quotes': [
-            'error',
-            'single'
-        ],
-        'semi': [
-            'error',
-            'never'
-        ]
-    }
+  env: {
+    browser: true,
+    commonjs: true,
+    es6: true
+  },
+  extends: "eslint:recommended",
+  parserOptions: {
+    sourceType: "module"
+  },
+  rules: {
+    "linebreak-style": "off",
+    indent: ["error", 2],
+    "no-console": "off",
+    quotes: ["error", "single"],
+    semi: ["error", "never"]
   }
+};
--- models/user.js
+++ models/user.js
@@ -20,7 +20,7 @@ const userSchema = new Schema({
     type: String
   },
   province: {
-    type: string
+    type: String
   }
 }, { collection: 'user', versionKey: false})
 

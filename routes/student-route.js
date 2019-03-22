0ab1b612e509745b135769a622e7068599767f58
yangpu <18312701884@163.com>
2019-03-22 22:25:33 +0800

    feat:更换学生的路由

--- routes/student-route.js
+++ routes/student-route.js
@@ -5,8 +5,8 @@ const studentController = require('../controllers/student-controller')
 
 router.post('/add', studentController.addStudent)
 router.post('/list', studentController.findStudentList)
-router.get('/detail/:id', studentController.findStudent)
-router.put('/detail/:id', studentController.updateStudent)
-router.delete('/detail/:id', studentController.delStudent)
+router.get('/:id', studentController.findStudent)
+router.put('/:id', studentController.updateStudent)
+router.delete('/:id', studentController.delStudent)
 
 module.exports = router

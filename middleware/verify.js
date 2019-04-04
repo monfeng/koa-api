b387a5d3390c9886064cba98024f8e707acfb4d8
yangpu <18312701884@163.com>
2019-04-04 23:03:39 +0800

    feat:优化token的验证

--- middleware/verify.js
+++ middleware/verify.js
@@ -2,28 +2,40 @@
 const  jwt = require('jsonwebtoken')
 const config = require('../config')
 const VerityUrl = 'auth'
+
 const VerifyToken = async(ctx, next) => {
-  if (ctx.method !== 'OPTIONS') {
-    const authorization = ctx.get('Authorization')
-    const url = ctx.path
-    console.log(url)
-    if (url.indexOf(VerityUrl) < 0) {
-      try {
-        const tokenContent = await jwt.verify(authorization, config.secret)
-        // 全局设置userId，数据库的查找
-        ctx.userId = tokenContent.userId
-      } catch (error) {
-        if ('TokenExpiredError' === error.name) {
-          ctx.throw(401, 'token expired！')
-        }
-        ctx.throw(401, 'invalid token')
-      }
-    }
+  // 如果是option请求过去
+  if (ctx.method === 'OPTIONS') {
     await next()
-  } else {
-    await next()
+    return
   }
   
+  // 如果是非token请求过去
+  const url = ctx.path
+  console.log(url)
+  if (url.indexOf(VerityUrl) > -1) {
+    await next()
+    return
+  }
+
+  const authorization = ctx.get('Authorization')
+  try {
+    const tokenContent = await jwt.verify(authorization, config.secret)
+    // 全局设置userId，数据库的查找
+    ctx.userId = tokenContent.userId
+    await next()
+  } catch (error) {
+    ctx.status = 401
+    const body = {
+      code: 0,
+      msg: 'TokenExpiredError' === error.name ? 'token expired' : 'invalid token',
+      desc: 'oken失效或者不合法，请重新登录页面'
+    }
+    ctx.body = body
+    // ctx.throw(403, 'token expired！')
+    // ctx.throw(403, 'invalid token')
+  }
+ 
 }
 
 module.exports = VerifyToken

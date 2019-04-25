cc6fc5f8dd9756137e1d17bfdc99c8007bac1789
yangpu <yangpu@xlink.com>
2019-04-25 11:33:26 +0800

    feat:添加mongosedb的密码

--- config/index.js
+++ config/index.js
@@ -1,7 +1,7 @@
 module.exports = {
   port: 3330, // 项目启动的端口
   secret: 'secret', // jwt加密的秘钥
-  db: 'mongodb://localhost:27017/koa-api', // 数据库
+  db: 'mongodb://koa-admin:131415@localhost:27017/koa-api', // 数据库
   saltTimes: 3, // 加密的次数（用户密码加密）
   prefix: '/v1' // api的前缀
 }
